fc9c2ac96e40d9289b6bd552f0888d3b85de3f73
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2023-11-24 11:48:16 +0100

    add nested create menu

--- scripts/Manage/Component-Manager.ps1
+++ scripts/Manage/Component-Manager.ps1
@@ -7,18 +7,43 @@ $scriptDirectory = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $MainMenuTitle = "Component Manager"
 $MainMenuOptions = @("Create Component", "Remove Component", "Exit")
 
-$MenuResult = New-Menu -MenuTitle $MainMenuTitle -MenuOptions $MainMenuOptions
+$CreateMenuTitle = "Create Component"
+$CreateMenuOptions = @("Single-File Component (SFC)", "Partial-File Split (PFS)", "Base-Class Split (BCS)", "Help", "Return")
 
-switch ($MenuResult) {
-    0 { 
-        Write-Host "create component..."
-        New-Menu -MenuTitle "Type" -MenuOptions @("")
+function MainMenu {
+    $MenuResult = New-Menu -MenuTitle $MainMenuTitle -MenuOptions $MainMenuOptions
+    switch ($MenuResult) {
+        0 { 
+            Write-Host "create component..."
+            $CreateMenuResult = New-Menu -MenuTitle $CreateMenuTitle -MenuOptions $CreateMenuOptions
+
+            switch ($CreateMenuResult) {
+                0 {
+                    Write-Host "SFC..."
+                }
+                1 {
+                    Write-Host "PFS..."
+                }
+                2 {
+                    Write-Host "BCS..."
+                }
+                3 {
+                    Write-Host "Help..."
+                }
+                4 {
+                    MainMenu
+                }
+            }
+        }
+        1 {
+            Write-Host "delete component..."
+        }
+        2 {
+            Write-Host "exit..."
+        }
+        Default {}
     }
-    1 {
-        Write-Host "delete component..."
-    }
-    2 {
-        Write-Host "exit..."
-    }
-    Default {}
 }
+
+MainMenu
+
